8d59c456bb1ba90307c49c42acbf0765f9298d09
John-thabuks <thabuksjohn@gmail.com>
2023-11-22 14:50:54 +0300

    Assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,22 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container
+        header="Example header"
+        direction="horizontal"
+        textPosition="right"
+      >
+        <section direction="vertical">
+          <div>This is an example!</div>
+          <div>This is an example!</div>
+          <div>This is an example!</div>
+          <div>This is an example!</div>
+          <div>This is an example!</div>
+          <div>This is an example!</div>
+          <div>This is an example!</div>
+        </section>
       </Container>
-      <Container header="Example header" textPosition="center">
+      <Container header="Example header" textPosition="left">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
@@ -33,6 +39,7 @@ function App() {
       </Container>
     </div>
   );
+  <section></section>;
 }
 
 export default App;
